98c1ded50418527b47b0b86b193a4ffedf71fbad
Unknown <cynthia.cjag@gmail.com>
2018-04-15 17:58:26 +0100

    Search with filters

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -10,5 +10,9 @@ class Restaurant < ApplicationRecord
   scope :restaurantByPostalCode, -> (postalCode) { Address.where("postalCode LIKE ?", postalCode) }
   scope :restaurantByName, -> (name) { Restaurant.where("name LIKE ?", name) }
   scope :restaurantByCity, -> (city) { Address.where("city LIKE ?", city) }
-  scope :restaurantByStreet, -> (street) { Address.where("street LIKE ?", street) }
+  scope :restaurantByStreet, -> (street) { Address.where("street LIKE ?", street)}
+  #Me devuelve los tipos de comida
+  scope :restaurantByFood, -> (name) { Type.where("name LIKE ?", name)}
+  scope :restaurantByType, -> (type) { Type.find_by_name(type)}
+
 end
