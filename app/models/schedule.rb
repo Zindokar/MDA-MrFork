1487fae00fb09665e43203ea5bb55b9c09fd1b21
Santiago Miguel Gubern González <migui1995@hotmail.es>
2018-05-06 17:52:49 +0100

    Restaurant show and edit pages

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,25 @@
 class Schedule < ApplicationRecord
   validates :weekDay, :openingTime, :closingTime, presence: true
   belongs_to :restaurant
+
+  default_scope -> {order('weekDay ASC')}
+
+  def dayToString
+    case weekDay
+    when 0
+      return "Lunes"
+    when 1
+      return "Martes"
+    when 2
+      return "Miércoles"
+    when 3
+      return "Jueves"
+    when 4
+      return "Viernes"
+    when 5
+      return "Sábado"
+    when 6
+      return "Domingo"
+    end
+  end
 end
