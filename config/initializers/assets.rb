50c23229cbdfd189e556abaf0874e81379380a70
Santiago Miguel Gubern González <migui1995@hotmail.es>
2018-05-07 12:37:40 +0100

    Styles fixes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
-# Be sure to restart your server when you modify this file.
+# Be sure to restart your server when you modify this typesfile.
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
@@ -14,3 +14,13 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( grid.css )
 Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( addresses.scss )
+Rails.application.config.assets.precompile += %w( alerts.scss )
+Rails.application.config.assets.precompile += %w( dishes.scss )
+Rails.application.config.assets.precompile += %w( photos.scss )
+Rails.application.config.assets.precompile += %w( reservations.scss )
+Rails.application.config.assets.precompile += %w( restaurants.scss )
+Rails.application.config.assets.precompile += %w( schedules.scss )
+Rails.application.config.assets.precompile += %w( searches.scss )
+Rails.application.config.assets.precompile += %w( types.scss )
+Rails.application.config.assets.precompile += %w( welcome.scss )
